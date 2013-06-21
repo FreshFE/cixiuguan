cb3ebdff1ed96fd8b4fd702117c86f22c3453581
ljh <xiao_dou_ok@163.com>
2013-06-21 17:27:17 +0800

    添加登陆实体层

---
+++ app/models/LoginModel.php
@@ -0,0 +1,37 @@
+<?php
+
+class LoginModel extends Eloquent
+{
+	/**
+	 * 管理员登陆
+	 *
+	 * 
+	 */
+	public static function adminLogin($username, $password)
+	{
+		$user = LoginModel::login($username,$password);
+ 		if($user) {
+ 			if(strlen($user[0]['username']) > 0) { 				
+ 				return $user;		
+ 			}
+ 		}
+ 		return false;
+	}
+
+	/**
+	 * 登陆
+	 *
+	 * 
+	 */
+	public static function login($username, $password)
+	{
+		$result = DB::table('user')->where('username', '=', $username)->get();
+		if (is_array($result) && count($result) > 0) {
+			if (strlen($password) > 0 && $password == $result[0]['password']) {
+				return $result;
+			}
+		}
+		return false;
+	}
+
+}
