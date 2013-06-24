2b640f4be9e357bec2f298a96e9c3e7c84223894
ljh <xiao_dou_ok@163.com>
2013-06-24 10:00:49 +0800

    添加签到控制层

---
+++ app/models/CheckinModel.php
@@ -0,0 +1,37 @@
+<?php
+
+class CheckinModel extends Eloquent
+{
+
+	/**
+	 * 得到签到内容 *
+	 *
+	 */
+	public static function getCheckin()
+	{
+		$result = DB::table('checkin')-> get();
+		return $result;
+	}
+
+	/**
+	 * 得到签到内容 *
+	 *
+	 *@param int $id
+	*/
+	public static function getCheckinById($id)
+	{
+		$result = DB::table('checkin')->where('id', '=', $id)->get();
+		return $result;
+	} 
+
+	/**
+	 * 删除签到内容 *
+	 *
+	 *@param int $id
+	*/
+	public static function deleteById($id)
+	{
+		$result = DB::table('checkin')->where('id', '=', $id)->delete();
+		return $result;
+	} 
+}
