09fbe5bf5e2e3df69a0582a5144d65fd1053fcb0
ljh <xiao_dou_ok@163.com>
2013-06-24 10:02:56 +0800

    修改注释

--- app/controllers/AnnouncementController.php
+++ app/controllers/AnnouncementController.php
@@ -2,56 +2,56 @@
 
 class AnnouncementController extends BaseController
 {
-	
 	/**
-	 * 获得公告所有数据
+	 * 显示布告数据 *
 	 *
 	 */
 	public  function index()
 	{
 		$result = AnnouncementModel::getAnnouncement();
+
 		if(is_array($result) && count($result)> 0 ) {
 			return View::make('announcement', array('announcementDetail' => $result));
 		} else {
-			echo "error";
+			echo 'error';
 		}
 	}
 
+	/**
+	 * 布告搜索功能 *
+	 *
+	 */
 	public function search()
 	{
-		$id = Input::get("id");
-
+		$id = Input::get('id');
 		return $this->show($id);
 	}
 
-
 	/**
-	 * 获得公告数据
-	 *
+	 * 根据ID 搜索布告	 *
 	 *@param int $aid
 	 */
 	public function show($id)
 	{
 		$result = AnnouncementModel::getAnnouncementById($id);
+
 		if(is_array($result) && count($result)> 0 ) {
 			return View::make('announcement', array('announcementDetail' => $result));
 		} else {
-			echo "error";
+			echo 'error';
 		}
 	}
 
-
 	/**
-	 * 插入公告数据
-	 *
+	 * 创建布告 *
+	 *	 
 	 */
 	public function create()
 	{
-
-		// 获得 post 数据传入,由于CSRF Protection,在接收的参数中多了一个_token参数，用except去除
+		//在接受的所有元素中，去除token元素
 		$inputData = Input::except('_token');
-
-		// 验证规则
+			
+		//数据验证
 		$validator = Validator::make($inputData, array(
 			'title' => array('required', 'max:100'),
 			'content' => array('required', 'max:500'),
@@ -59,38 +59,67 @@ class AnnouncementController extends BaseController
 		));
 
 		if ($validator->fails()) {
-			return "error";
+			return 'error';
 	    } else {
 	    	$result = AnnouncementModel::insertAnnouncement($inputData);
 	    	if($result == true) {
 	    		return Redirect::action('AnnouncementController@index');
 	    	} else {
-	    		return "error";
+	    		return 'error';
 	    	}
 	    }
 	}
 
-
 	/**
-	 * 删除公告数据
-	 *
+	 * 删除布告	 *
 	 *@param int $id
 	 */
 	public function destory($id)
 	{ 
 		$result = AnnouncementModel::deleteById($id);
+
 		if($result > 0){
 			return Redirect::action('AnnouncementController@index');
 		} else {
-			echo "error";
+			echo 'error';
 		}
 	}
 
-
-	public function edit($announcementStr){
-		$result = explode(",", $announcementStr);
-		var_dump($result);
-		exit();
-		//return View::make('announcement', array('announcementDetail' => $result));
+	/**
+	 * 修改布告字段，跳转到修改页面 *
+	 *@param int $id
+	 */
+	public function edit($id)
+	{
+		$result = AnnouncementModel::getAnnouncementById($id);
+		
+		if(is_array($result) && count($result)> 0 ) {
+			return View::make('announcementUpdate', array('announcementDetail' => $result));
+		} else {
+			echo 'error';
+		}
 	}
+	
+	/**
+	 * 修改布告字段 *
+	 *
+	 */
+	public function put()
+	{
+		$id = Input::get('id');
+
+		$data = array(
+			'title' => 	 Input::get('title'),
+			'content' => Input::get('content'),
+		);
+		
+		$result = AnnouncementModel::updateById($id, $data);
+		
+		if ($result > 0) {
+			return Redirect::action('AnnouncementController@index');
+		} else {
+			return "error";
+		}		
+	}
+	
 }
