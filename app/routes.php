8b5adf1347857d19129c8c92c6143a22c96beb9c
ljh <xiao_dou_ok@163.com>
2013-06-24 10:01:16 +0800

    添加签到路由

--- app/routes.php
+++ app/routes.php
@@ -12,42 +12,44 @@
 */
 
 /**
- * API 部分
+ * API 閮ㄥ垎
  */
 Route::group(array('prefix' => 'api'), function()
 {
-	// 签到创建接口（含评价内容）
-	Route::post('checkin', 'ApiCheckinController@create');
+	// 绛惧埌鍒涘缓鎺ュ彛锛堝惈璇勪环鍐呭锛�	Route::post('checkin', 'ApiCheckinController@create');
 
-	// 获得景点签到数量
+	// 鑾峰緱鏅偣绛惧埌鏁伴噺
 	Route::get('checkin/{place_id}', 'ApiCheckinController@show');
 
-	// 获得景点签到的评价
-	Route::get('checkin/{place_id}/comments', 'ApiCheckinController@comments');
+	// 鑾峰緱鏅偣绛惧埌鐨勮瘎浠�	Route::get('checkin/{place_id}/comments', 'ApiCheckinController@comments');
 
-	// 获得公告列表
+	// 鑾峰緱鍏憡鍒楄〃
 	Route::get('announcement', 'ApiAnnouncementController@index');
 
-	// 获得公告详情
+	// 鑾峰緱鍏憡璇︽儏
 	Route::get('announcement/{announcement_id}', 'ApiAnnouncementController@show');
 });
 
 /**
- * 管理员部分
- */
+ * 绠＄悊鍛橀儴鍒� */
 Route::group(array('prefix' => 'admin'), function()
 {
-	Route::get('announcement', 'AnnouncementController@index');//获得公告列表
-	Route::post('announcement', 'AnnouncementController@create');//添加公告
+	Route::get('announcement', 'AnnouncementController@index');//鑾峰緱鍏憡鍒楄〃
+	Route::post('announcement', 'AnnouncementController@create');//娣诲姞鍏憡
 	Route::post('announcement/search', 'AnnouncementController@search'); 
 	Route::get('announcement/{id}', 'AnnouncementController@show');
-	Route::get('announcement/{id}/delete', 'AnnouncementController@destory');//删除一个公告
-	Route::get('announcement/{id}/edit', 'AnnouncementController@put');
+	Route::get('announcement/{id}/delete', 'AnnouncementController@destory');//鍒犻櫎涓�釜鍏憡
+	Route::post('announcement/edit', 'AnnouncementController@put');
+	Route::get('announcement/{id}/edit', 'AnnouncementController@edit');
+	
+	Route::get('checkin', 'CheckinController@index');
+	Route::post('checkin/search', 'CheckinController@search');
+	Route::get('checkin/{id}', 'CheckinController@show');
+	Route::get('checkin/{id}/delete', 'CheckinController@destory');
 });
 
 /**
- * 根目录
- */
+ * 鏍圭洰褰� */
 Route::get('/', function()
 {
 	return View::make('hello');
@@ -55,8 +57,7 @@ Route::get('/', function()
 
 
 /**
- * 管理员登陆
- */
+ * 绠＄悊鍛樼櫥闄� */
 Route::get('login', 'LoginController@index');
 Route::post('loginSuccess', 'LoginController@loginSuccess');
 
