55ee96ab6577d4a458dabc1db985bb46cde8173b
ljh <xiao_dou_ok@163.com>
2013-06-25 16:44:02 +0800

    增加一行空格

--- app/models/LoginModel.php
+++ app/models/LoginModel.php
@@ -9,6 +9,7 @@ class LoginModel extends Eloquent
 	public static function adminLogin($username, $password)
 	{
 		$user = LoginModel::login($username,$password);
+
  		if($user) {
  			if(strlen($user[0]['username']) > 0) { 				
  				return $user;		
@@ -24,6 +25,7 @@ class LoginModel extends Eloquent
 	public static function login($username, $password)
 	{
 		$result = DB::table('user')->where('username', '=', $username)->get();
+		
 		if (is_array($result) && count($result) > 0) {
 			if (strlen($password) > 0 && $password == $result[0]['password']) {
 				return $result;
