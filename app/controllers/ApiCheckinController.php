d2a8158b06ad174354a961132d31cebf688d3c65
ljh <xiao_dou_ok@163.com>
2013-06-25 16:44:28 +0800

    删除临时测试数据

--- app/controllers/ApiCheckinController.php
+++ app/controllers/ApiCheckinController.php
@@ -4,38 +4,36 @@ class ApiCheckinController extends BaseController
 {
 	/**
 	 * 签到创建接口（含评价内容）
-	 *
 	 * 输入内容由 post 提交的数据处理
+	 *
+	 * @param form post
+	 * @return string json
 	 */
 	public function create()
 	{
-		// 获得 post 数据传入
-		$data = Input::all();
-
-		// 测试数据，临时
+		// 通过 Input post 方式获得数据
 		$data = array(
-			'place_id' => 6,
-			'lat' => '22.532987',
-			'lng' => '22.532987',
-			'comments' => '988okokokokokokokok',
-			'stars' => 3,
-
+			'place_id' => Input::get('place_id'),
+			'lat' => Input::get('lat'),
+			'lng' => Input::get('lng'),
+			'comments' => Input::get('comments'),
+			'stars' => Input::get('stars'),
+			'create_at' => date('Y-m-d H:i')
 		);
 
 		// 验证规则
 		$validator = Validator::make($data, array(
 			'place_id' =>  array('required', 'numeric'),
-			'lat' => array('required'),
-			'lng' => array('required'),
 			'comments' => array('required', 'max:500'),
 			'stars' => array('required', 'integer')
 		));
 
+		// 验证失败
 		if ($validator->fails()) {
-			// 验证失败
-			return $this->errorJson($validator->messages());
-	    } else {
-	    	// 验证成功，插入数据库
+	    	return $this->errorJson($validator->messages()->first());
+	    }
+	    // 验证成功，插入数据库
+	    else {
 			DB::table('checkin')->insert($data);
 			return $this->successJson($data);
 	    }
@@ -48,8 +46,16 @@ class ApiCheckinController extends BaseController
 	 */
 	public function show($place_id)
 	{
-		$data = DB::table('checkin')->where('place_id', '=', $place_id)->count();
-		return $this->successJson($data);
+		$data['counts'] = DB::table('checkin')->where('place_id', '=', $place_id)->count();
+		
+		// 得到checkin的stars字段，这是一个数组，array(1,2,3,4,1,3)，求平均数，加入$data
+		$data['starsAvg'] = round(DB::table('checkin')->where('place_id', '=', $place_id)->avg('stars'));
+
+		if (empty($data['starsAvg'])) {
+			$data['starsAvg'] = '3';
+		}
+
+ 		return $this->successJson($data);
 	}
 
 	/**
