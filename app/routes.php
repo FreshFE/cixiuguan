376866ab812a15447c788d3af5a34b40f1f67544
Mino Wu <minowu@foxmail.com>
2013-07-01 11:16:42 +0800

    优化routes

--- app/routes.php
+++ app/routes.php
@@ -16,20 +16,26 @@
  */
 Route::group(array('prefix' => 'api'), function()
 {
-	// 签到创建接口（含评价内容）
-	Route::post('checkin', 'ApiCheckinController@create');
+	Route::group(array('prefix' => 'checkin'), function()
+	{
+		// 签到创建接口（含评价内容）
+		Route::post('/', 'ApiCheckinController@create');
 
-	// 获得景点签到数量
-	Route::get('checkin/{place_id}', 'ApiCheckinController@show');
+		// 获得景点签到数量
+		Route::get('{place_id}', 'ApiCheckinController@show');
 
-	// 获得景点签到的评价
-	Route::get('checkin/{place_id}/comments', 'ApiCheckinController@comments');
+		// 获得景点签到的评价
+		Route::get('{place_id}/comments', 'ApiCheckinController@comments');
+	});
 
-	// 获得公告列表
-	Route::get('announcement', 'ApiAnnouncementController@index');
+	Route::group(array('prefix' => 'announcement'), function()
+	{
+		// 获得公告列表
+		Route::get('/', 'ApiAnnouncementController@index');
 
-	// 获得公告详情
-	Route::get('announcement/{announcement_id}', 'ApiAnnouncementController@show');
+		// 获得公告详情
+		Route::get('{announcement_id}', 'ApiAnnouncementController@show');
+	});
 });
 
 /**
@@ -37,35 +43,30 @@ Route::group(array('prefix' => 'api'), function()
  */
 Route::group(array('prefix' => 'admin'), function()
 {
+	// 公告
+	Route::group(array('prefix' => 'announcement'), function()
+	{
+		Route::get('/', 'AnnouncementController@index');
+		Route::get('create', 'AnnouncementController@getCreate');
+		Route::post('create', 'AnnouncementController@postCreate');
+		Route::get('search', 'AnnouncementController@search');
+		Route::get('{id}/edit', 'AnnouncementController@getEdit');
+		Route::post('{id}/edit', 'AnnouncementController@postEdit');
+		Route::get('{id}', 'AnnouncementController@show');
+		Route::get('{id}/delete', 'AnnouncementController@destory');
+	});
+	
+	// 签到
+	Route::group(array('prefix' => 'checkin'), function()
+	{
+		Route::get('/', 'CheckinController@index');
+		Route::get('search', 'CheckinController@search');
+		Route::get('{id}', 'CheckinController@show');
+		Route::get('{id}/delete', 'CheckinController@destory');
+	});
+
 	// 后台首页
 	Route::get('/', 'AdminController@index');
-
-	//布告列表
-	Route::get('announcement', 'AnnouncementController@index');
-
-	//布告创建
-	Route::post('announcement', 'AnnouncementController@create');
-
-	//布告搜索
-	Route::post('announcement/search', 'AnnouncementController@search'); 
-	Route::get('announcement/{id}', 'AnnouncementController@show');
-
-	//布告删除
-	Route::get('announcement/{id}/delete', 'AnnouncementController@destory');
-
-	//布告更新
-	Route::post('announcement/edit', 'AnnouncementController@put');
-	Route::get('announcement/{id}/edit', 'AnnouncementController@edit');
-	
-	//签到列表
-	Route::get('checkin', 'CheckinController@index');
-
-	//签到搜索
-	Route::get('checkin/search', 'CheckinController@search');
-	Route::get('checkin/{id}', 'CheckinController@show');
-
-	//签到删除
-	Route::get('checkin/{id}/delete', 'CheckinController@destory');
 });
 
 /**
@@ -84,29 +85,3 @@ Route::get('login', 'LoginController@index');
 
 //登陆成功跳转
 Route::post('loginSuccess', 'LoginController@loginSuccess');
-
-Route::get('admin/test/checkin', function()
-{
-	return View::make('admin/checkin/index');
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
